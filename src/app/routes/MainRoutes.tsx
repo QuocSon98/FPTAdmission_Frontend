ab49a5d8ad2bac9f339fa3b61f8ad229c849a7e4
datndse171888 <datndse171888@fpt.edu.vn>
2025-05-31 21:04:58 +0700

    add: contract function
    + add src\app\assets\img03.png
    + add contract.tsx, contract.css, admission.tsx
    + fix MainRoutes.tsx, layout.tsx, header.tsx

--- src/app/routes/MainRoutes.tsx
+++ src/app/routes/MainRoutes.tsx
@@ -2,14 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../pages/authentication";
 import AdminHome from "../pages/adminHome";
 import Register from "../pages/authentication/register";
-import Home from"../layouts/layout";
+import Layout from"../layouts/layout";
+import Home from "../pages/public/homePage/home";
+import Contract from "../pages/public/contract/contract";
 
 const MainRoutes = () => {
     return (
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Home/>} />
+                    <Route path="/" element={<Layout/>}>
+                        <Route index element={<Home/>} />
+                        <Route path="/contract" element={<Contract/>} />
+                    </Route>
                     <Route path="/login" element={<Login/>} />
                     <Route path="/admin" element={<AdminHome/>} />
                     <Route path="/register" element={<Register />} />
